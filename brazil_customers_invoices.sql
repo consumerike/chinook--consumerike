7c53f696947b23a1bd24668a86df390d1389085e
Ikechi <consumerike@gmail.com>
2017-03-28 09:31:24 -0500

    completed query

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,9 @@
+/*Provide a query showing the Invoices of customers who are from Brazil. 
+The resultant table should show the customer's full name, 
+Invoice ID, Date of the invoice and billing country.*/
+
+SELECT Invoice.InvoiceId, customer.FirstName, customer.LastName, Invoice.InvoiceDate, Invoice.BillingCountry
+FROM Customer
+JOIN Invoice
+ON Customer.CustomerId = Invoice.CustomerId
+WHERE Customer.Country = 'Brazil';
